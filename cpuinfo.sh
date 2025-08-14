ac513586ef42c35ffc9359af5f8552453bf0a680
Szerwigi1410 <szerwigi14@gmail.com>
2025-08-14 19:13:40 +0200

    Update cpuinfo.sh

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -2,3 +2,15 @@
 
 # From the creator of Brokefetch (Oliwier)
 # I can proudly present cpuinfo!
+
+GREEN="\033[32m"
+RED="\033[31m"
+BLUE="\033[34m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+YELLOW="\033[33m"
+PURPLE="\033[35m"
+BOLD="\033[1m"
+RESET="\033[0m"
+BLACK="\033[30m"
+GRAY="\033[90m"
