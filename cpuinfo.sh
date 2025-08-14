013734ab761dca5f09a0375ac61d7f9eab4985d5
Szerwigi1410 <szerwigi14@gmail.com>
2025-08-14 19:32:27 +0200

    Update cpuinfo.sh

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -14,3 +14,35 @@ BOLD="\033[1m"
 RESET="\033[0m"
 BLACK="\033[30m"
 GRAY="\033[90m"
+
+# CONFIG
+CONFIG_FILE="$HOME/.config/brokefetch/config"
+
+# If there is no config – create a default one.
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    mkdir -p "$(dirname "$CONFIG_FILE")"
+    echo -e "# Available COLOR_NAME options: RED, GREEN, BLUE, CYAN, WHITE, YELLOW, PURPLE, BLACK, GRAY" > "$CONFIG_FILE"
+	echo -e "COLOR_NAME=BLUE" >> "$CONFIG_FILE"
+fi
+
+# Load values from the config
+source "$CONFIG_FILE"
+
+## Info gathering
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS_NAME="$NAME"
+    OS_VERSION="$VERSION"
+else
+    OS_NAME="$(uname -s)"
+    OS_VERSION="$(uname -r)"
+fi
+
+CPU_NAME="$(lscpu | grep 'Model name' | awk -F: '{print $2}' | xargs)"
+
+## Dispplayer!
+
+echo -e "OS: ${GREEN}${OS_NAME} ${OS_VERSION}${RESET}"
+echo -e "Architecture: ${CYAN}$(uname -m)${RESET}"
+echo -e "CPU model: ${BLUE}${CPU_NAME}${RESET}"
+echo -e "Cores: ${YELLOW}$(nproc)${RESET}"
